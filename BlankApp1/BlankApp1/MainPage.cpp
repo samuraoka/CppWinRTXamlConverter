202e5daa09120e9eac1b3b6162e442ea8a717a87
Satoshi Muraoka <sa.muraoka@outlook.com>
2020-01-16 03:41:21 +0900

    a button was placed in the main page to update a internal value.

--- BlankApp1/BlankApp1/MainPage.cpp
+++ BlankApp1/BlankApp1/MainPage.cpp
@@ -20,5 +20,22 @@ namespace winrt::BlankApp1::implementation
     void MainPage::MyProperty(int32_t value)
     {
         m_MyProperty = value;
+        OnPropertyChanged(*this, PropertyChangedEventArgs{ L"MyProperty" });
     }
+
+    winrt::event_token MainPage::PropertyChanged(Windows::UI::Xaml::Data::PropertyChangedEventHandler const& handler)
+    {
+        return OnPropertyChanged.add(handler);
+    }
+    void MainPage::PropertyChanged(winrt::event_token const& token) noexcept
+    {
+        OnPropertyChanged.remove(token);
+    }
+}
+
+void winrt::BlankApp1::implementation::MainPage::IncrementButton_Click(
+    winrt::Windows::Foundation::IInspectable const& /*sender*/,
+    winrt::Windows::UI::Xaml::RoutedEventArgs const& /*e*/)
+{
+    MyProperty(m_MyProperty + 1);
 }
--- BlankApp1/BlankApp1/MainPage.h
+++ BlankApp1/BlankApp1/MainPage.h
@@ -11,9 +11,15 @@ namespace winrt::BlankApp1::implementation
         int32_t MyProperty();
         void MyProperty(int32_t value);
 
+        winrt::event_token PropertyChanged(Windows::UI::Xaml::Data::PropertyChangedEventHandler const& handler);
+        void PropertyChanged(winrt::event_token const& token) noexcept;
+
+        void IncrementButton_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::UI::Xaml::RoutedEventArgs const& e);
+
     private:
         int32_t m_MyProperty{ 0 };
-    };
+        winrt::event<winrt::Windows::UI::Xaml::Data::PropertyChangedEventHandler> OnPropertyChanged;
+	};
 }
 
 namespace winrt::BlankApp1::factory_implementation
