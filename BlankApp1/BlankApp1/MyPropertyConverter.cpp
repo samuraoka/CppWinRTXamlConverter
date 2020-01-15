3ae9a512036eda63dfb51082c7501307f2fd6fbd
Satoshi Muraoka <sa.muraoka@outlook.com>
2020-01-16 03:43:32 +0900

    some files were added for a converter runtime class.

---
+++ BlankApp1/BlankApp1/MyPropertyConverter.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "MyPropertyConverter.h"
+#if __has_include("MyPropertyConverter.g.cpp")
+#include "MyPropertyConverter.g.cpp"
+#endif
+
+namespace winrt::BlankApp1::implementation
+{
+    int32_t MyPropertyConverter::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MyPropertyConverter::MyProperty(int32_t /*value*/)
+    {
+        throw hresult_not_implemented();
+    }
+}
---
+++ BlankApp1/BlankApp1/MyPropertyConverter.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "MyPropertyConverter.g.h"
+
+namespace winrt::BlankApp1::implementation
+{
+    struct MyPropertyConverter : MyPropertyConverterT<MyPropertyConverter>
+    {
+        MyPropertyConverter() = default;
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+    };
+}
+
+namespace winrt::BlankApp1::factory_implementation
+{
+    struct MyPropertyConverter : MyPropertyConverterT<MyPropertyConverter, implementation::MyPropertyConverter>
+    {
+    };
+}
